5055e3a1ee42683b3c445cbbb0d9ef70ce54117a
Nick Sandel <nsandel@rm.com>
2023-02-22 16:40:51 +0000

    Solved problem 36 - maybe my quickest yet! Saving the is_palindrome function helped a lot as did a quick google on how to get to binary from int

---
+++ Thirties/problem36.py
@@ -0,0 +1,20 @@
+# The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+# Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+# (Please note that the palindromic number, in either base, may not include leading zeros.)
+
+import sys
+ 
+# setting path
+sys.path.append('../Euler')
+from common.helpers import is_palindrome
+
+bit_pals = []
+for i in range(1, 1000000):
+    if is_palindrome(i):
+        bit_version = int(f"{i:b}")
+        if is_palindrome(bit_version):
+            bit_pals.append(i)
+
+print(sum(bit_pals))
