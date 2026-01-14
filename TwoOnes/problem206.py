a64f402f42382dec00a73d387a23f5a4bc209731
Nick Sandel <nick.sandel@tetratech.uk>
2026-01-14 08:53:16 +0000

    Tidied up as I'd gone with 1s instead of 0s to find the lower bound

--- TwoOnes/problem206.py
+++ TwoOnes/problem206.py
@@ -5,17 +5,17 @@ from math import sqrt
 print(sqrt(1929394959697989990))
 #1389026623.1062636
 
-print(sqrt(1121314151617181910))
-#1058921220.685081
+print(sqrt(1020304050607080900))
+#1010101010.1010101
 
 # Ok so the number is in this range. I could brute force one way or another to find it...
 # For brute force it's just a case of iterating through those place numbers until a whole number is found from rooting it
 # Or is it less combinations to go through the range I have?
 # 1,389,026,623
 
-print(1389026623 - 1058921220)
+print(1389026623 - 1010101010)
 # 330,105,403
-# Half this as the number ends in zero so can't be from an odd number
+# One tenth of this
 # The other way round is 9! which is 362,880 so actually that way will be much more efficient
 
 # I could write a horribly messy nested loop 9 layers deep but would rather not frankly
@@ -31,12 +31,12 @@ def check_square_form(number):
         return False
     
 print(check_square_form(1929394959697989990))
-print(check_square_form(1121314151617181910))
+print(check_square_form(1020304050607080900))
 print(check_square_form(1121314151617181919))
 
 # Trying to figure out the permutations is giving me a headache so I'll brute force between the range instead!
 
-start = 1058921220
+start = 1010101010
 end = 1389026623
 
 while start < end:
@@ -48,3 +48,6 @@ while start < end:
     start += 10 # 10 as the resulting number must end in 0
 
 # This was a bit slow but still got the right result
+
+# Winner:
+# 1389019170
